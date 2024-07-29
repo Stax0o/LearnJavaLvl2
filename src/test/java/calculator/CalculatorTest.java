92ae7291367d789834f08a2dd96ed6a34dbd2bf6
Artem Aliev <stax0o.devr@gmail.com>
2024-07-29 18:43:36 +0500

    Add tests

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,43 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void addSimple() {
+        int expected = 15;
+        int result = Calculator.add(10, 5);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void addAnotherSimple() {
+        int expected = 48;
+        int result = Calculator.add(39, 9);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void subtractSimple() {
+        int expected = 44;
+        int result = Calculator.subtract(108, 64);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void multiplySimple() {
+        int expected = 125;
+        int result = Calculator.multiply(25, 5);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void divideSimple() {
+        double expected = 5.5625;
+        double result = Calculator.divide(89, 16);
+        assertEquals(expected, result, 0.001);
+    }
+}
