b7d0549b5caf6be336d957d9c1ef3eba908a77d1
Artem Aliev <stax0o.devr@gmail.com>
2024-08-06 15:28:31 +0500

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,6 @@
 package main;
 
-import studyingExceptions.Calculator;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Calculator.add(3, 4));
     }
 }
