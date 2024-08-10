ac0a5fc656feebc4b3582aa887f37002da343646
Artem Aliev <stax0o.devr@gmail.com>
2024-08-10 17:53:22 +0500

    Update CarCollection.java

--- src/main/java/studyingCollection/CarCollection.java
+++ src/main/java/studyingCollection/CarCollection.java
@@ -4,12 +4,8 @@ import studyingArrayList.Car;
 
 public interface CarCollection extends Iterable<Car> {
     boolean add(Car car);
-
     boolean remove(Car car);
-
     int size();
-
     void clear();
-
     boolean contains(Car car);
 }
