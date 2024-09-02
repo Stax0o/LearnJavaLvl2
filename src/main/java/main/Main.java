f7561d0b7929e28b9ae6be5b018f4033b737d95f
Artem Aliev <stax0o.devr@gmail.com>
2024-09-02 13:29:22 +0500

    feat: Studying thread synchronization

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,42 @@
 package main;
 
+import concurrency.ATMMachine;
+
 public class Main {
     public static void main(String[] args) {
+        ATMMachine atm = new ATMMachine();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                atm.cashOut("Bob", 1_500);
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                atm.cashOut("John", 890);
+            }
+        });
+
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                atm.cashOut("Harry", 870_000);
+            }
+        });
+
+        Thread thread4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                atm.cashOut("Oliver", 100_000);
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
     }
 }
