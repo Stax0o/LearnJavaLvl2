39b6c88bfc78c7c5cd649b59105cd55346aeca3c
Artem Aliev <stax0o.devr@gmail.com>
2024-09-06 17:07:42 +0500

    A study of stream

--- src/main/java/stream/Filter.java
+++ src/main/java/stream/Filter.java
@@ -29,14 +29,4 @@ public class Filter {
                 .limit(5)
                 .forEach(System.out::println);
     }
-
-//    private static List<Integer> filter(List<Integer> list, Predicate predicate) {
-//        List<Integer> result = new ArrayList<>();
-//        for (int i : list) {
-//            if (predicate.test(i)) {
-//                result.add(i);
-//            }
-//        }
-//        return result;
-//    }
 }
