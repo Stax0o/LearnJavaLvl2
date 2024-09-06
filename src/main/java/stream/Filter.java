6aa137cbfd5c50c4e33b43c497fc9ba1652061f9
Artem Aliev <stax0o.devr@gmail.com>
2024-09-06 11:51:47 +0500

    A study of stream

--- src/main/java/stream/Filter.java
+++ src/main/java/stream/Filter.java
@@ -3,30 +3,36 @@ package stream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Filter {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < 1000; i++) {
-            list.add(random.nextInt(100));
+            list.add(random.nextInt(100, 200));
         }
 
-        List<Integer> listFiltered;
-        listFiltered = filter(list, a -> a % 3 == 0 || a % 5 == 0);
+        List<String> list2 = list.stream()
+                .filter(n -> n % 2 == 0 && n % 5 == 0)
+                .map(Math::sqrt)
+                .map(scrt ->"Sqrt: " + scrt)
+                .collect(Collectors.toList());
 
-        for (Integer i : listFiltered) {
-            System.out.println(i);
+        for (String s : list2) {
+            System.out.println(s);
         }
+        System.out.println(list2.size());
+
     }
 
-    private static List<Integer> filter(List<Integer> list, Predicate predicate) {
-        List<Integer> result = new ArrayList<>();
-        for (int i : list) {
-            if (predicate.test(i)) {
-                result.add(i);
-            }
-        }
-        return result;
-    }
+//    private static List<Integer> filter(List<Integer> list, Predicate predicate) {
+//        List<Integer> result = new ArrayList<>();
+//        for (int i : list) {
+//            if (predicate.test(i)) {
+//                result.add(i);
+//            }
+//        }
+//        return result;
+//    }
 }
